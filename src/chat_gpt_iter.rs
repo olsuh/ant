128d6a8bad79ac58ad93c518de9af624d2725ec3
olsuh <olsuh@ya.ru>
2023-02-23 16:53:45 +0200

    four algoritms

--- src/chat_gpt_iter.rs
+++ src/chat_gpt_iter.rs
@@ -1,3 +1,5 @@
+use crate::{sum_digits, Int};
+
 pub fn main() {
     let start_time = std::time::Instant::now(); // засекаем время начала работы
 
@@ -5,26 +7,31 @@ pub fn main() {
     visited.insert((1000, 1000)); // добавляем начальную позицию муравья
 
     let mut ant_pos = (1000, 1000); // текущая позиция муравья
-    let sum_digits = |n:i32| { // функция для подсчета суммы цифр числа
+    /*let sum_digits = |n:i32| { // функция для подсчета суммы цифр числа
         n.to_string().chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>()
-    };
+    };*/
+    let mut rem = Vec::new();
 
     loop {
         // перемещаем муравья в новую клетку, выбирая доступные направления
-        let available_moves: Vec<(i32, i32)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)]
+        let mut available_moves: Vec<(i32, i32)> = vec![(0, 1), (0, -1), (1, 0), (-1, 0)]
             .iter()
             .map(|(dx, dy)| (ant_pos.0 + dx, ant_pos.1 + dy))
-            .filter(|(x, y)| sum_digits(*x) + sum_digits(*y) <= 25 && !visited.contains(&(*x, *y)))
+            .filter(|(x, y)| !visited.contains(&(*x, *y)) && sum_digits(*x as Int) + sum_digits(*y as Int) <= 25 )
             .collect();
 
         if available_moves.is_empty() {
-            break; // если нет доступных клеток, выходим из цикла
+            match rem.pop(){
+                Some(a_m) => available_moves.push(a_m),
+                None => break, // если нет доступных клеток, выходим из цикла
+            }
         }
 
-        ant_pos = available_moves[0]; // перемещаем муравья в первую доступную клетку
+        ant_pos = available_moves.pop().unwrap(); // перемещаем муравья в первую доступную клетку
+        rem.append(&mut available_moves);
         visited.insert(ant_pos); // добавляем ее в множество посещенных клеток
     }
 
-    let elapsed_time = start_time.elapsed(); // измеряем время работы
+    let elapsed_time = start_time.elapsed().as_secs_f32(); // измеряем время работы
     println!("Муравей посетил {} клеток за {:?}", visited.len(), elapsed_time);
 }
